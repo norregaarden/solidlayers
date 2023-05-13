c78ed35b5df3b304d8926d8a4a3b5466a42c5dd5
Johan Nørregaard Andersen <jna.jelling@gmail.com>
2023-05-13 22:37:17 +0200

    maybe solid devtools

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import solid from "solid-start/vite";
 import { defineConfig } from "vite";
+import solid from "solid-start/vite";
+import devtools from 'solid-devtools/vite'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    devtools({
+    
+// autoname:true
+    }),
+    solid()],
+  // build:{target:'esnext'}
 });
